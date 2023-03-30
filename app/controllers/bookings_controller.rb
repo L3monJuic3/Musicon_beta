b7c5a35729add3f3f9fe28d91e81442d4301c019
ethan lane <ethan.lane@outlook.co.nz>
2023-03-30 15:17:57 +0100

    updated class names, css, and bookings controller code as per review notes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,13 @@
 class BookingsController < ApplicationController
+  before_action set_lesson:, only: %i[new create]
+
   def index
     @booking = Booking.all
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.lesson_id = @lesson.id
     @booking.user = current_user
     if @booking.save
       redirect_to lesson_booking_path(@lesson.id, @booking.id)
@@ -25,10 +28,10 @@ class BookingsController < ApplicationController
   private
 
   def set_lesson
-    @lesson = Lesson.find(:Lesson_id)
+    @lesson = Lesson.find(params[lesson_id])
   end
 
   def booking_params
-    params.require(:booking).permit(:booking_date, is_cancelled:, slot_id:)
+    params.require(:booking).permit(:booking_date, :is_cancelled, :slot_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lessons do
-    resources :booking
+    resources :bookings, only: %i[create destroy]
   end
   # Defines the root path route ("/")
   # root "articles#index"
